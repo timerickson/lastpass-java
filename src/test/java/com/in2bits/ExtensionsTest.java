63c0d74519b3bf0545c03f4657df3ca898f71560
Timothy Erickson <Tim@TimBookPro.local>
2017-07-25 21:34:58 -0700

    Javafied a couple more test classes
    * Should prolly start executing some of the tests soon

--- src/test/java/com/in2bits/ExtensionsTest.java
+++ src/test/java/com/in2bits/ExtensionsTest.java
@@ -3,12 +3,26 @@ package com.in2bits;
 // Copyright (C) 2013 Dmitry Yakimenko (detunized@gmail.com).
 // Licensed under the terms of the MIT license. See LICENCE for details.
 
+import com.in2bits.adapters.Base64Decoder;
+import com.in2bits.shims.Action;
+import com.in2bits.shims.Ref;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 class ExtensionsTest
 {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     public void Reverse()
     {
@@ -20,103 +34,118 @@ class ExtensionsTest
         assertEquals(0xdeadbeefL, Extensions.Uint.reverse(0xefbeadde));
     }
 
+    private long bytesToLong(byte[] bytes) {
+        ByteBuffer buffer = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN);
+        buffer.put(bytes, 0, Long.SIZE);
+        return buffer.getLong();
+    }
+
     @Test
     public void FromBigEndian()
     {
-        assertEquals(0L, BitConverter.ToUInt32(new byte[] {0x00, 0x00, 0x00, 0x00}, 0).FromBigEndian());
-        assertEquals(0x12345678L, BitConverter.ToUInt32(new byte[] {0x12, 0x34, 0x56, 0x78}, 0).FromBigEndian());
-        assertEquals(0xdeadbeefL, BitConverter.ToUInt32(new byte[] {0xde, 0xad, 0xbe, 0xef}, 0).FromBigEndian());
+        assertEquals(0L, Extensions.Uint.fromBigEndian(bytesToLong(new byte[] {0x00, 0x00, 0x00, 0x00})));
+        assertEquals(0x12345678L, Extensions.Uint.fromBigEndian(bytesToLong(new byte[] {0x12, 0x34, 0x56, 0x78})));
+        assertEquals(0xdeadbeefL, Extensions.Uint.fromBigEndian(bytesToLong(new byte[] {(byte) 0xDE, (byte) 0xad, (byte) 0xbe, (byte) 0xef})));
     }
 
     @Test
     public void ToUtf8()
     {
-        assertEquals("", new byte[] {}.ToUtf8());
-        assertEquals(_helloUtf8, _helloUtf8Bytes.ToUtf8());
+        assertEquals("", Extensions.Bytes.toUtf8(new byte[] {}));
+        assertEquals(_helloUtf8, Extensions.Bytes.toUtf8(_helloUtf8Bytes));
     }
 
     @Test
     public void ToHex()
     {
-        foreach (var i in _hexToBytes)
-        assertEquals(i.Key, i.Value.ToHex());
+        for (Map.Entry<String, byte[]> i : _hexToBytes.entrySet()) {
+            assertEquals(i.getKey(), Extensions.Bytes.toHex(i.getValue()));
+        }
     }
 
     @Test
-    public void ToBytes()
+    public void ToBytes() throws Exception
     {
-        assertEquals(new byte[] {}, "".ToBytes());
-        assertEquals(_helloUtf8Bytes, _helloUtf8.ToBytes());
+        assertEquals(new byte[] {}, Extensions.Strings.toBytes(""));
+        assertEquals(_helloUtf8Bytes, Extensions.Strings.toBytes(_helloUtf8));
     }
 
     @Test
     public void DecodeHex()
     {
-        foreach (var i in _hexToBytes)
+        for (Map.Entry<String, byte[]> i : _hexToBytes.entrySet())
         {
-            assertEquals(i.Value, i.Key.DecodeHex());
-            assertEquals(i.Value, i.Key.ToUpper().DecodeHex());
+            assertEquals(i.getValue(), Extensions.Strings.decodeHex(i.getKey()));
+            assertEquals(i.getValue(), Extensions.Strings.decodeHex(i.getKey().toUpperCase()));
         }
     }
 
     @Test
-        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "Input length must be multple of 2")]
     public void DecodeHex_throws_on_odd_length()
     {
-        "0".DecodeHex();
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Input length must be multple of 2");
+        Extensions.Strings.decodeHex("0");
     }
 
     @Test
-        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "Input contains invalid characters")]
     public void DecodeHex_throws_on_non_hex_characters()
     {
-        "xz".DecodeHex();
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Input contains invalid characters");
+        Extensions.Strings.decodeHex("xz");
     }
 
     @Test
     public void Decode64()
     {
-        assertEquals(new byte[] {}, "".Decode64());
-        assertEquals(new byte[] {0x61}, "YQ==".Decode64());
-        assertEquals(new byte[] {0x61, 0x62}, "YWI=".Decode64());
-        assertEquals(new byte[] {0x61, 0x62, 0x63}, "YWJj".Decode64());
-        assertEquals(new byte[] {0x61, 0x62, 0x63, 0x64}, "YWJjZA==".Decode64());
+        Base64Decoder decoder = new TestBase64Decoder();
+        assertEquals(new byte[] {}, Extensions.Strings.decode64(decoder, ""));
+        assertEquals(new byte[] {0x61}, Extensions.Strings.decode64(decoder, "YQ=="));
+        assertEquals(new byte[] {0x61, 0x62}, Extensions.Strings.decode64(decoder, "YWI="));
+        assertEquals(new byte[] {0x61, 0x62, 0x63}, Extensions.Strings.decode64(decoder, "YWJj"));
+        assertEquals(new byte[] {0x61, 0x62, 0x63, 0x64}, Extensions.Strings.decode64(decoder, "YWJjZA=="));
     }
 
     @Test
     public void Times()
     {
-        var times = new int[] {0, 1, 2, 5, 10};
-        foreach (var i in times)
+        int[] times = new int[] {0, 1, 2, 5, 10};
+        for (Integer i : times)
         {
-            var called = 0;
-            i.Times(() => ++called);
-            assertEquals(i, called);
+            final Ref<Integer> calledRef = new Ref<>();
+            calledRef.setValue(0);
+            Extensions.Integers.times(i, new Action(){
+                @Override
+                public void execute() {
+            calledRef.setValue(calledRef.getValue()+1);}});
+            assertEquals(i, calledRef.getValue());
         }
     }
 
-    private readonly string _helloUtf8 = "Hello, UTF-8!";
-    private readonly byte[] _helloUtf8Bytes = new byte[] {
+    private final String _helloUtf8 = "Hello, UTF-8!";
+    private final byte[] _helloUtf8Bytes = new byte[] {
         0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2C, 0x20, 0x55, 0x54, 0x46, 0x2D, 0x38, 0x21
 };
 
-    private readonly Dictionary<string, byte[]> _hexToBytes = new Dictionary<string, byte[]> {
-    {"",
-            new byte[] {}},
+    private final static Map<String, byte[]> _hexToBytes = new HashMap<String, byte[]>();
+    static {
+        _hexToBytes.put("",
+                new byte[] {});
 
-    {"00",
-            new byte[] {0}},
+        _hexToBytes.put("00",
+                new byte[] {0});
 
-    {"00ff",
-            new byte[] {0, 255}},
+        _hexToBytes.put("00ff",
+                new byte[] {0, (byte)255});
 
-    {"00010203040506070809",
-            new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+        _hexToBytes.put("00010203040506070809",
+                new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
 
-    {"000102030405060708090a0b0c0d0e0f",
-            new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}},
+        _hexToBytes.put("000102030405060708090a0b0c0d0e0f",
+                new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
 
-    {"8af633933e96a3c3550c2734bd814195",
-            new byte[] {0x8A, 0xF6, 0x33, 0x93, 0x3E, 0x96, 0xA3, 0xC3, 0x55, 0x0C, 0x27, 0x34, 0xBD, 0x81, 0x41, 0x95}}
-};
+        _hexToBytes.put("8af633933e96a3c3550c2734bd814195",
+                new byte[] {(byte) 0x8A, (byte) 0xF6, 0x33, (byte) 0x93, 0x3E, (byte) 0x96, (byte) 0xA3, (byte) 0xC3, 0x55, 0x0C, 0x27, 0x34, (byte) 0xBD, (byte) 0x81, 0x41, (byte) 0x95});
+    }
 }
